7ce8b30ab52d8c067c2f5963caf517511c7df9d6
Jake Stone <Stonedev12@gmail.com>
2021-10-05 20:41:48 -0700

    User can move outside of car

--- test.js
+++ test.js
@@ -3,7 +3,11 @@ var data = {
   view: 'mainMenu',
   x: 0,
   y: 0,
-  speed: 3
+  speed: 3,
+  userX: 0,
+  userY: 0,
+  userSpeed: 1,
+  exitCar: false
 };
 
 var stateJSON = JSON.stringify(data);
@@ -19,8 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
   if (data.view === 'mainMenu') {
     mainMenu.style.display = 'flex';
     gameScreen.style.display = 'none';
-  }
-  if (data.view === 'game') {
+  } else if (data.view === 'game') {
     mainMenu.style.display = 'none';
     gameScreen.style.display = 'flex';
   }
@@ -48,8 +51,7 @@ document.addEventListener('click', function (event) {
     stateJSON = JSON.stringify(data);
     localStorage.setItem('javascript-local-storage', stateJSON);
     localStorage.getItem('javascript-local-storage');
-  }
-  if (event.target === pick2 || event.target === carBox2) {
+  } else if (event.target === pick2 || event.target === carBox2) {
     data.x = 0;
     data.y = 0;
     car.setAttribute('src', 'images/customcar.svg');
@@ -60,8 +62,7 @@ document.addEventListener('click', function (event) {
     stateJSON = JSON.stringify(data);
     localStorage.setItem('javascript-local-storage', stateJSON);
     localStorage.getItem('javascript-local-storage');
-  }
-  if (event.target === pick3 || event.target === carBox3) {
+  } else if (event.target === pick3 || event.target === carBox3) {
     data.x = 0;
     data.y = 0;
     car.setAttribute('src', 'images/fastback.svg');
@@ -75,67 +76,109 @@ document.addEventListener('click', function (event) {
   }
 });
 
+// game
 document.addEventListener('keydown', handleDirection);
 var car = document.getElementById('car');
+var user = document.getElementById('user');
+
 function handleDirection(event) {
   if (event.key === 'w') {
     car.className = 'north';
-  }
-  if (event.key === 's') {
+    user.className = 'north';
+  } else if (event.key === 's') {
     car.className = 'south';
-  }
-  if (event.key === 'd') {
+    user.className = 'south';
+  } else if (event.key === 'd') {
     car.className = 'east';
-  }
-  if (event.key === 'a') {
+    user.className = 'east';
+  } else if (event.key === 'a') {
     car.className = 'west';
+    user.className = 'west';
   }
 }
 
 var carEvent = {
   on: false
 };
+var userEvent = {
+  on: false
+};
 
 function move() {
   if (car.className === 'east') {
     data.x = data.x + data.speed;
-  }
-  if (car.className === 'south') {
+  } else if (car.className === 'south') {
     data.y = data.y + data.speed;
-  }
-  if (car.className === 'west') {
+  } else if (car.className === 'west') {
     data.x = data.x - data.speed;
-  }
-  if (car.className === 'north') {
+  } else if (car.className === 'north') {
     data.y = data.y - data.speed;
   }
   car.style.left = data.x + 'px';
   car.style.top = data.y + 'px';
 }
 
+function moveUser() {
+  if (user.className === 'east') {
+    data.userX = data.userX + data.userSpeed;
+  } else if (user.className === 'south') {
+    data.userY = data.userY + data.userSpeed;
+  } else if (user.className === 'west') {
+    data.userX = data.userX - data.userSpeed;
+  } else if (user.className === 'north') {
+    data.userY = data.userY - data.userSpeed;
+  }
+  user.style.left = data.userX + 'px';
+  user.style.top = data.userX + 'px';
+}
+
 var interval;
+var userInterval;
 document.addEventListener('keydown', function (event) {
-
-  if (event.code === 'Space' && carEvent.on === false) {
-    interval = setInterval(function () {
-      move();
-    }, 14);
-
-    carEvent.on = true;
-  } else if (event.code === 'Space' && carEvent.on === true) {
+  if (event.key === 't') {
     carEvent.on = false;
     clearInterval(interval);
+    data.exitCar = true;
   }
-  if (event.code === 'KeyY') {
+  if (data.exitCar === false) {
+    if (event.code === 'Space' && carEvent.on === false) {
+      interval = setInterval(function () {
+        move();
+      }, 14);
+
+      carEvent.on = true;
+    } else if (event.code === 'Space' && carEvent.on === true) {
+      carEvent.on = false;
+      clearInterval(interval);
+    }
+  }
+
+  if (data.exitCar === true) {
+    if (event.code === 'Space' && userEvent.on === false) {
+      userInterval = setInterval(function () {
+        moveUser();
+      }, 14);
+
+      carEvent.on = true;
+    } else if (event.code === 'Space' && userEvent.on === true) {
+      userEvent.on = false;
+      clearInterval(userInterval);
+    }
+  } else if (event.code === 'KeyY') {
     mainMenu.style.display = 'flex';
     gameScreen.style.display = 'none';
     data.x = 0;
     data.y = 0;
     car.style.top = '0';
     car.style.left = '0';
+    user.style.top = '0';
+    user.style.left = '0';
+    car.className = 'east';
+    user.className = 'east';
+    carEvent.on = false;
+    clearInterval(interval);
     data.view = 'mainMenu';
     stateJSON = JSON.stringify(data);
     localStorage.setItem('javascript-local-storage', stateJSON);
   }
-})
-;
+});
